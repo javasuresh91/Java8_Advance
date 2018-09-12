150de95de780b9bba239a61dc902bc4c3d970e9b
Suresh <javasuresh91@gmail.com>
2018-09-12 18:10:15 +0530

    Code commit

---
+++ EX_002_FunctionalInterfaceWithLambda/src/com/demo/sample/functional/inter/FunctionalInterfaceSeven.java
@@ -0,0 +1,12 @@
+package com.demo.sample.functional.inter;
+
+/**
+ * 
+ * It is also functional interface internally because it has only one abstract
+ * method
+ * 
+ */
+public interface FunctionalInterfaceSeven {
+	int add();
+
+}
